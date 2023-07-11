99f8bc0f253d01463f366ee1b9c7e98c97099e57
Sintel <sintel.inquiries@gmail.com>
2023-07-11 17:41:35 +0200

    make delay nicer

--- rust/effect/delay.rs
+++ rust/effect/delay.rs
@@ -1,12 +1,10 @@
 use crate::dsp::delayline::DelayLine;
-use crate::dsp::smooth::{SmoothExp, SmoothLinear};
+use crate::dsp::smooth::{SmoothBuffer, SmoothExp};
 use crate::dsp::*;
 use crate::effect::Effect;
 
 // max length in seconds
 const MAX_LEN: f32 = 1.0;
-
-// TODO: run LFO at block rate
 
 #[derive(Debug)]
 pub struct Delay {
@@ -25,72 +23,68 @@ struct Track {
 	delay: SmoothExp,
 	delay2: SmoothExp,
 	lfo_accum: f32,
-	lfo_phase: f32,
-	lfo_freq: f32,
-	lfo_mod: SmoothLinear,
+	left: bool,
+	lfo: SmoothBuffer,
 	delayline: DelayLine,
+	dc_killer: DcKiller,
 }
 
 impl Track {
-	pub fn new(sample_rate: f32, lfo_phase: f32) -> Self {
+	pub fn new(sample_rate: f32, left: bool) -> Self {
 		Track {
 			delay: SmoothExp::new(200.0, sample_rate),
 			delay2: SmoothExp::new(200.0, sample_rate),
 			delayline: DelayLine::new(sample_rate, MAX_LEN),
 			lfo_accum: 0.,
-			lfo_phase,
-			lfo_freq: 0.,
-			lfo_mod: SmoothLinear::new(50.0, sample_rate),
+			left,
+			lfo: SmoothBuffer::new(),
+			dc_killer: DcKiller::new(sample_rate),
 		}
-	}
-}
-
-impl Delay {
-	fn update_delay(&mut self) {
-		let multiplier = pow2_cheap(self.offset);
-		self.tracks[0].delay.set(self.time * multiplier);
-		self.tracks[1].delay.set(self.time / multiplier);
-	}
-
-	fn update_lfo(&mut self) {
-		self.tracks.iter_mut().for_each(|track| {
-			track.lfo_freq = self.lfo_freq / self.sample_rate;
-			track.lfo_mod.set(0.002 * self.lfo_mod / self.lfo_freq);
-		})
 	}
 }
 
 impl Effect for Delay {
 	fn new(sample_rate: f32) -> Self {
 		Delay {
-			tracks: [Track::new(sample_rate, 0.), Track::new(sample_rate, 0.25)],
+			tracks: [
+				Track::new(sample_rate, true),
+				Track::new(sample_rate, false),
+			],
 			sample_rate,
 			balance: 0.,
 			time: 0.,
 			offset: 0.,
 			feedback: 0.,
-			lfo_freq: 0.,
+			lfo_freq: 0.5,
 			lfo_mod: 0.,
 		}
 	}
 
 	fn process(&mut self, buffer: &mut [&mut [f32]; 2]) {
 		for (buf, track) in buffer.iter_mut().zip(self.tracks.iter_mut()) {
-			for sample in buf.iter_mut() {
+			let n = buf.len();
+
+			// update modulation lfo
+			track.lfo_accum += (self.lfo_freq / self.sample_rate) * n as f32;
+			track.lfo_accum -= track.lfo_accum.floor();
+
+			let lfo_mod = 0.002 * self.lfo_mod / self.lfo_freq;
+			let phase = if track.left { 0. } else { 0.25 };
+			let lfo = lfo_mod * sin_cheap(track.lfo_accum + phase);
+			track.lfo.set(lfo);
+			track.lfo.process_buffer(n);
+
+			for (i, sample) in buf.iter_mut().enumerate() {
 				let input = *sample;
 
-				track.lfo_accum += track.lfo_freq;
-				let lfo_mod = track.lfo_mod.process();
-
-				let lfo = lfo_mod * sin_cheap(track.lfo_accum + track.lfo_phase);
-
 				// delay is smoothed twice to get a continuous derivative
-				let delay = track.delay.process();
-				track.delay2.set(delay);
-				let delay2 = track.delay2.process();
-				let s = track.delayline.go_back_linear(delay2 + lfo);
+				track.delay2.set(track.delay.process());
+				let delay = track.delay2.process();
+				let s = track.delayline.go_back_linear(delay + track.lfo.get(i));
 
-				track.delayline.push(input + self.feedback * s);
+				track.delayline.push(softclip_cubic(
+					track.dc_killer.process(input + self.feedback * s),
+				));
 				*sample = lerp(input, s, self.balance);
 			}
 		}
@@ -107,15 +101,18 @@ impl Effect for Delay {
 				self.update_delay();
 			}
 			3 => self.feedback = value,
-			4 => {
-				self.lfo_freq = value;
-				self.update_lfo();
-			}
-			5 => {
-				self.lfo_mod = value;
-				self.update_lfo();
-			}
+			4 => self.lfo_freq = value,
+			5 => self.lfo_mod = value,
+
 			_ => eprintln!("Parameter with index {index} not found"),
 		}
+	}
+}
+
+impl Delay {
+	fn update_delay(&mut self) {
+		let multiplier = pow2_cheap(self.offset);
+		self.tracks[0].delay.set(self.time * multiplier);
+		self.tracks[1].delay.set(self.time / multiplier);
 	}
 }
