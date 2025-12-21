56c79348434ed5d04b3c2a5d101bb127cb0282f2
Sintel <sintel.inquiries@gmail.com>
2025-12-21 03:25:37 +0100

    formatting lua/lib/*

--- lua/lib/strict.lua
+++ lua/lib/strict.lua
@@ -5,21 +5,21 @@ local mt = {}
 setmetatable(_G, mt)
 
 mt.__newindex = function(t, n, v)
-    if debug.getinfo(2) then
-        local w = debug.getinfo(2, "S").what
-        -- local src = debug.getinfo(2, "S").source
-        -- if w ~= "main" and w ~= "C" or src ~= "@src\\main.lua" then
+	if debug.getinfo(2) then
+		local w = debug.getinfo(2, "S").what
+		-- local src = debug.getinfo(2, "S").source
+		-- if w ~= "main" and w ~= "C" or src ~= "@src\\main.lua" then
 
-        if w ~= "main" then
-            error("Script attempted to create global variable '" .. tostring(n) .. "'", 2)
-        end
-    end
-    rawset(t, n, v)
+		if w ~= "main" then
+			error("Script attempted to create global variable '" .. tostring(n) .. "'", 2)
+		end
+	end
+	rawset(t, n, v)
 end
 
 mt.__index = function(t, n)
-    if debug.getinfo(2) and debug.getinfo(2, "S").what ~= "C" then
-        error("Script attempted to access unexisting global variable '" .. tostring(n) .. "'", 2)
-    end
-    return rawget(t, n)
+	if debug.getinfo(2) and debug.getinfo(2, "S").what ~= "C" then
+		error("Script attempted to access unexisting global variable '" .. tostring(n) .. "'", 2)
+	end
+	return rawget(t, n)
 end
