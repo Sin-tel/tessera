746832858ae8227855c3f7d322e2450afac7a9a7
Sintel <sintel.inquiries@gmail.com>
2026-01-05 22:35:45 +0100

    add new icon

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ use winresource::WindowsResource;
 fn main() -> io::Result<()> {
 	println!("cargo:rerun-if-changed=build.rs");
 	println!("cargo:rerun-if-changed=assets/icon.ico");
+	println!("cargo:rerun-if-changed=assets/icon.png");
 
 	// Only run on Windows builds
 	if env::var_os("CARGO_CFG_TARGET_OS") == Some("windows".into()) {
